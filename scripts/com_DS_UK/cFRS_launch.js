7d9203dba9d6ba50c33bf1da2342a30b6984041c
natanprog <natan10@ymail.com>
2020-04-30 21:12:38 -0300

    Adicionado Filter, Rename, and Share commands com_DS_UK

---
+++ scripts/com_DS_UK/cFRS_launch.js
@@ -0,0 +1,6 @@
+script="https://media.innogamescdn.com/com_DS_UK/Scripts/cFRS.js";
+s=document.createElement('script');
+s.type='text/javascript';
+s.src=script+'?'+Math.floor((new Date()).valueOf()/604800000 + 6);
+document.getElementsByTagName('head')[0].appendChild(s);
+void 0;
