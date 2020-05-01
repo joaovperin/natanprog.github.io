754376e1749fbddabb41de86578cce3600192569
natanprog <natan10@ymail.com>
2020-05-01 00:50:05 -0300

    Adicionado Script para responder a mensagens pré-definidas no fórum da tribo com_DS_PT

---
+++ scripts/com_DS_PT/forum.js
@@ -0,0 +1,6 @@
+﻿if (document.URL.indexOf('&screen=forum') == -1) {
+  var x=window.confirm('Você deve executar o script no \n\n Fórum da tribo em novo tópico ou em responder a uma mensagem sendo que neste caso só introduz o campo da mensagem. \n\n Redireccionar para Fórum da Tribo?')
+if (x)
+window.location="game.php?&screen=forum";
+}else{$('input[name^=subject]').val(''+titulo+'');$('#message').val(''+texto+'');
+}void(0);
