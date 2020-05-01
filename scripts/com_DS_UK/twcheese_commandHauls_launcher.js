75b472b7a115a844650260c4c6060640cb5e21ad
natanprog <natan10@ymail.com>
2020-04-30 21:53:45 -0300

    Adicionado show hauls on commands overview com_DS_UK

---
+++ scripts/com_DS_UK/twcheese_commandHauls_launcher.js
@@ -0,0 +1,5 @@
+javascript:ukScript="https://media.innogamescdn.com/com_DS_UK/Scripts/twcheese_commandHauls.js";
+s=document.createElement('script');
+s.type='text/javascript';
+s.src=ukScript+'?'+Math.floor((new Date()).valueOf()/604800000 +2);
+document.getElementsByTagName('head')[0].appendChild(s);
