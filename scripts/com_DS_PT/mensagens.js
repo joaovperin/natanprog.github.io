ed291e805094aa99af3a3d10a14a5e7a1be5185d
natanprog <natan10@ymail.com>
2020-05-01 00:58:43 -0300

    Adicionado Script para criar mensagens pré-definidas com_DS_PT

---
+++ scripts/com_DS_PT/mensagens.js
@@ -0,0 +1,6 @@
+﻿if (document.URL.indexOf('&screen=mail') == -1) {
+  var x=window.confirm('Você deve executar o script em \n\n Nova mensagem ou em responder a uma mensagem sendo que neste caso só introduz o campo da mensagem. \n\n Redireccionar para mensagens?')
+if (x)
+window.location="game.php?&screen=mail";
+}else{$('input[name^=subject]').val(''+assunto+'');$('#message').val(''+mensagem+'');
+}void(0);
