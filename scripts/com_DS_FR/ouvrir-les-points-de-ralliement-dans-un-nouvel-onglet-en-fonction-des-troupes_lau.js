f947f6ae2f00c904241e78a766de9a55431b636b
natanprog <natan10@ymail.com>
2020-05-06 23:39:19 -0300

    Adicionado Ouvrir les points de ralliement dans un nouvel onglet. com_DS_FR

---
+++ scripts/com_DS_FR/ouvrir-les-points-de-ralliement-dans-un-nouvel-onglet-en-fonction-des-troupes_lau.js
@@ -0,0 +1,1 @@
+﻿$.ajax({dataType:'script',cache:true,url:'https://media.innogamescdn.com/com_DS_FR/Scripts/Attaque/ouvrir-les-points-de-ralliement-dans-un-nouvel-onglet-en-fonction-des-troupes2.js'});
