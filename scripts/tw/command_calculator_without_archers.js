8d14d3cb4e07a9e9d87bb0c01ffa54446032596d
natanprog <natan10@ymail.com>
2020-05-18 18:17:19 -0300

    Traduzindo para o Português/Inglês

--- scripts/tw/command_calculator_without_archers.js
+++ scripts/tw/command_calculator_without_archers.js
@@ -1,15 +1,90 @@
+/*
+         This script...
+
+         scriptname:       Kalkulator wojsk idących na daną wioskę
+         author:           Howcio712
+         editor:           Rinne, Gangsta Anime Girl
+         original url:     https://media.innogamescdn.com/com_DS_PL/skrypty/wojska_idace_na_wioske.js
+
+         editor:           natanprog
+         url:              https://natanprog.github.io/scripts/tw/
+*/
 javascript:
   if (window.location.href.match("info_village") != null){
       var unitlink = "https://dspl.innogamescdn.com/asset/63a05504/graphic/unit/unit_";
+var langScript = [];
+switch(game_data.locale) {
+  case "pt_BR":
+    langScript = [
+        "Calculadora de Comandos",
+        "Próprios comandos",
+        "Todos os comandos",
+        "Carregar dados",
+        "Tipo",
+        "Criado por",
+        "Reparado por",
+        "Tropas a caminho",
+        "Ofensivas:",
+        "Defensivas:",
+        "Outras:",
+        "Bárbaros: ",
+        "CL: ",
+        "AC: ",
+        "Aríetes: ",
+        "Lanceiros: ",
+        "Espadachins: ",
+        "Arqueiros: ",
+        "CP: ",
+        "Fazendas DEF: ",
+        "Exploradores: ",
+        "Catapultas: ",
+        "Paladinos: ",
+        "Nobres: ",
+        "DEF na aldeia",
+        "Fazendas: ",
+        "versão para mundos sem arqueiros"
+    ]
+    break;
+  default:
+    langScript = [
+        "Command Calculator",
+        "Own commands",
+        "All commands",
+        "Load data",
+        "Type",
+        "Created by",
+        "Repaired by",
+        "Troops on their way",
+        "Offensive:",
+        "Defensive:",
+        "Others:",
+        "Axes: ",
+        "LC: ",
+        "MA: ",
+        "Rams: ",
+        "Spears: ",
+        "Swords: ",
+        "Archers: ",
+        "HC: ",
+        "DEF Farms: ",
+        "Scouts: ",
+        "Cats: ",
+        "Pallys: ",
+        "Nobles: ",
+        "DEF in the village",
+        "Farms: ",
+        "Version for worlds without archers"
+    ]
+}      
 var jednostkiObrazki = "spear,sword,axe,spy,light,heavy,ram,catapult,knight,snob".split(",");
-var tabelka= '<style>#hofcio_div td{white-space: nowrap;} #hofcio_suma_off,#hofcio_suma_def,#hofcio_suma_o,#hofcio_suma_in div{margin:0px 5px 5px 0px;} h4{color:green;} tr{border-spacing: 2px;border-collapse: separate;}</style>';
-tabelka +='<div id="hofcio_div" style="width:750px;"><h2>Kalkulator rozkazów</h2>'+
-'<div style="width:68%;float:left;overflow: auto; height: 350px;">'+
-'<select id="howcio_sel"><option value=0>Tylko własne komendy</option><option value=1>Wszystkie komendy</option></select> <input type=button id="howcio_subm" class=btn value="Załaduj Dane" onclick="letsgo();"><br><br>'+
-'<table class="vis hofcio" id=tabelkahow><tbody><tr class=zostaw>';
-for(var a=0;a<jednostkiObrazki.length;a++){    tabelka += '<th width="25px"><img src="'+unitlink+jednostkiObrazki[a]+'.png"></th>'}
-tabelka +='<th>Typ</th></tr></tbody></table></div>';
-tabelka +='<div style="width:31%;float:right;"><div style="width:100% text-align:center;"><h4 id="hofcio_suma_1h"></h4><div id="hofcio_suma_off" style ="width:45%; float:left;"></div><div id="hofcio_suma_def" style ="width:45%; float:right;"></div></div><div id="hofcio_suma_o"></div><div id="hofcio_suma_in"></div><div style="position: fixed;bottom: 0;right: 0;fontSize:8px">Created by <b>Howcio712</b> / Repaired by <b>Rinne</b> & <b>Gangsta Anime Girl</b><div style="right: 0;fontSize:8px; color:green;"><b>Wersja dla światów bez łuczników</b></div></div></div>';
+var tabelka= "<style>#hofcio_div td{white-space: nowrap;} #hofcio_suma_off,#hofcio_suma_def,#hofcio_suma_o,#hofcio_suma_in div{margin:0px 5px 5px 0px;} h4{color:green;} tr{border-spacing: 2px;border-collapse: separate;}</style>";
+tabelka +="<div id='hofcio_div' style='width:750px;'><h2>" + langScript[0] + "</h2>"+
+"<div style='width:68%;float:left;overflow: auto; height: 350px;'>"+
+"<select id='howcio_sel'><option value=0>" + langScript[1] + "</option><option value=1>" + langScript[2] + "</option></select> <input type=button id='howcio_subm' class=btn value='" + langScript[3] + "' onclick='letsgo();'><br><br>"+
+"<table class='vis hofcio' id=tabelkahow><tbody><tr class=zostaw>";
+for(var a=0;a<jednostkiObrazki.length;a++){    tabelka += "<th width='25px'><img src='"+unitlink+jednostkiObrazki[a]+".png'></th>"}
+tabelka +="<th>" + langScript[4] + "</th></tr></tbody></table></div>";
+tabelka +="<div style='width:31%;float:right;'><div style='width:100% text-align:center;'><h4 id='hofcio_suma_1h'></h4><div id='hofcio_suma_off' style ='width:45%; float:left;'></div><div id='hofcio_suma_def' style ='width:45%; float:right;'></div></div><div id='hofcio_suma_o'></div><div id='hofcio_suma_in'></div><div style='position: fixed;bottom: 0;right: 0;fontSize:8px'>" + langScript[5] + " <b>Howcio712</b><br>" + langScript[6] + " <b>Rinne</b> & <b>Gangsta Anime Girl</b><div style='right: 0;fontSize:8px; color:green;'><b>" + langScript[26] + "</b></div></div></div>";
 Dialog.show("okienko_komunikatu",tabelka);
 function letsgo(){
     document.getElementById("howcio_subm").disabled=true;
@@ -33,8 +108,8 @@ setTimeout(function(){
     document.getElementById("howcio_subm").disabled=false;
 },2500)};
     if(game_data.player.sitter != 0){
-var url = 'game.php?screen=info_command&t=' + game_data.player.id;      
-}else{var url = 'game.php?screen=info_command';}
+var url = "game.php?screen=info_command&t=" + game_data.player.id;      
+}else{var url = "game.php?screen=info_command";}
 if(ideki.length>0){  
 var numerek = ideki[0].split(":")[0];
 var tekst = ideki[0].split(":")[1];
@@ -92,9 +167,17 @@ for (var i = 1;i<linijkii.length;i++)
 var razemzag = Math.round(((((1*tablicax[1])+(1*tablicax[2])+(6*tablicax[6]))/20000))* 100)/100;
 if (razemzag == undefined){razemzag=0;};
 }
-$("#hofcio_suma_1h")[0].innerHTML = "Wojsko w drodze na wioskę";
-$("#hofcio_suma_off")[0].innerHTML = "<font color=red>Ofensywa:</font><br>Topornik: <b>"+hofcio_sum[2]+"</b><br>LK: <b>"+hofcio_sum[4]+"</b><br>Taran: <b>"+hofcio_sum[6]+"</b>";
-$("#hofcio_suma_def")[0].innerHTML = "<font color=red>Defensywa:</font><br>Pikinier: <b>"+hofcio_sum[0]+"</b><br>Miecznik: <b>"+hofcio_sum[1]+"</b><br>CK: <b>"+hofcio_sum[5]+"</b><br><br>Razem Zagród DEF: <b>"+zagrody+"</b>";
-$("#hofcio_suma_o")[0].innerHTML = "<font color=red>Inne:</font><br>Zwiadowca: <b>"+hofcio_sum[3]+"</b><br>Katapulta: <b>"+hofcio_sum[7]+"</b><br>Rycerz: <b>"+hofcio_sum[8]+"</b><br>Szlachcic: <b>"+hofcio_sum[9]+"</b>";
-$("#hofcio_suma_in")[0].innerHTML = "<h4>Def znajdujący się w wiosce</h4>Pikinier: <b>"+tablicax[1]+"</b><br>Miecznik: <b>"+tablicax[2]+"</b><br>CK: <b>"+tablicax[6]+"</b><br>Razem Zagród <b>"+razemzag+"</b><br>Katapulta: <b>"+tablicax[8]+"</b><br>Rycerz: <b>"+tablicax[9]+"</b><br>";
-}}else{UI.InfoMessage('Uruchom w informacjach o wiosce<br>(Środkowa ikonka klikając na wioskę na mapie)<br><img src="https://help.plemiona.pl/images/4/46/Mapa_-_Menu_-_Informacje_o_wiosce.png">',8000,'error')};
+$("#hofcio_suma_1h")[0].innerHTML = langScript[7];
+$("#hofcio_suma_off")[0].innerHTML = "<font color=red>" + langScript[8] + "</font><br>" + langScript[11] + "<b>"+hofcio_sum[2]+"</b><br>" + langScript[12] + "<b>"+hofcio_sum[4]+"</b><br>" + langScript[14] + "<b>"+hofcio_sum[6]+"</b>";
+$("#hofcio_suma_def")[0].innerHTML = "<font color=red>" + langScript[9] + "</font><br>" + langScript[15] + "<b>"+hofcio_sum[0]+"</b><br>" + langScript[16] + "<b>"+hofcio_sum[1]+"</b><br>" + langScript[18] + "<b>"+hofcio_sum[5]+"</b><br><br>" + langScript[19] + "<b>"+zagrody+"</b>";
+$("#hofcio_suma_o")[0].innerHTML = "<font color=red>" + langScript[10] + "</font><br>" + langScript[20] + "<b>"+hofcio_sum[3]+"</b><br>" + langScript[21] + "<b>"+hofcio_sum[7]+"</b><br>" + langScript[22] + "<b>"+hofcio_sum[8]+"</b><br>" + langScript[23] + "<b>"+hofcio_sum[9]+"</b>";
+$("#hofcio_suma_in")[0].innerHTML = "<h4>" + langScript[24] + "</h4>" + langScript[15] + "<b>"+tablicax[1]+"</b><br>" + langScript[16] + "<b>"+tablicax[2]+"</b><br>" + langScript[18] + "<b>"+tablicax[6]+"</b><br>" + langScript[25] + "<b>"+razemzag+"</b><br>" + langScript[21] + "<b>"+tablicax[8]+"</b><br>" + langScript[22] + "<b>"+tablicax[9]+"</b><br>";
+}}else{
+    switch(game_data.locale) {
+        case "pt_BR":
+        UI.InfoMessage("Use na Informação da aldeia<br>(Ícone do meio clicando na aldeia no mapa)<br><img src='https://natanprog.github.io/images/mp_info.png'>",8000,"error");
+        break;
+        default:
+        UI.InfoMessage("Use in the Village Information<br>(Middle icon by clicking on the village on the map)<br><img src='https://natanprog.github.io/images/mp_info.png'>",8000,"error");
+    }
+};
