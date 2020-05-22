aa398d82577ee14ccb0a6a54c5b9d74b71302982
natanprog <natan10@ymail.com>
2020-05-22 11:22:20 -0300

    Atualizado Tribal stats Shinko to Kuma

--- scripts/com_DS_NET/shinkotokuma/tribeStats.js
+++ scripts/com_DS_NET/shinkotokuma/tribeStats.js
@@ -1,11 +1,45 @@
 javascript:
 
 
-if (window.location.href.indexOf('screen=ally&mode=members') < 0) {
+/*if (window.location.href.indexOf('screen=ally&mode=members') < 0) {
     //relocate
     window.location.assign(game_data.link_base_pure + "ally&mode=members");
-}
+}*/
 // Sophie "Shinko to Kuma"
+var tribeTable='';
+var rowStart=1;
+var columnStart=6;
+var columnName=0;
+var rows;
+if (window.location.href.indexOf('screen=ally&mode=members') >-1)
+{
+    //members own tribe
+    tribeTable= "#content_value table.vis";
+    rowStart=3;
+    columnStart=6;
+    columnName=0;
+    rows = $($("table .vis")[2]).find('tr');
+}
+if  (window.location.href.indexOf('&screen=info_ally') >-1)
+{
+    //any tribe
+    tribeTable= ".vis:eq(2)";
+    rowStart=1;
+    columnStart=5;
+    columnName=0;
+    rows = $($("table .vis")[2]).find('tr');
+}
+if (window.location.href.indexOf('screen=ranking&mode=player') >-1 || (window.location.href.indexOf('screen=ranking') >-1 && window.location.href.indexOf('&mode') == -1))
+{
+    //any player
+    tribeTable= ".vis:eq(1)";
+    rowStart=1;
+    columnStart=6;
+    columnName=1;
+    rows = $($("table .vis")[1]).find('tr');
+}
+
+
 $.getAll = function (
     urls, // array of URLs
     onLoad, // called when any URL is loaded, params (index, data)
@@ -48,7 +82,7 @@ $.getAll = function (
     }
 };
 var names = [];
-rows = $($("table .vis")[2]).find('tr');
+
 for (var i = 1; i < rows.length; i++) {
     names.push($(rows[i]).find('a')[0].innerText.trim().split(' ').join('+'));
 }
@@ -57,18 +91,21 @@ linksODS = [];
 linksODD = [];
 linksODA = [];
 linksLoot = [];
+linksGathering = [];
 ODSperPlayer = [];
 ODDperPlayer = [];
 ODAperPlayer = [];
 lootperPlayer = [];
+gatheredperPlayer = [];
 for (var i = 0; i < names.length; i++) {
     linksODA.push("/game.php?screen=ranking&mode=in_a_day&type=kill_att&name=" + names[i]);
     linksODS.push("/game.php?screen=ranking&mode=in_a_day&type=kill_sup&name=" + names[i]);
     linksODD.push("/game.php?screen=ranking&mode=in_a_day&type=kill_def&name=" + names[i]);
     linksLoot.push("/game.php?screen=ranking&mode=in_a_day&type=loot_res&name=" + names[i]);
+    linksGathering.push("/game.php?screen=ranking&mode=in_a_day&type=scavenge&name=" + names[i]);
 }
-$("#content_value table.vis tr").eq(2).append("<th onclick='sortTableTest(6)'>ODA</th><th onclick='sortTableTest(7)'>ODD</th><th onclick='sortTableTest(8)'>ODS</th><th onclick='sortTableTest(9)'>Loot</th>")
-$("#content_value table.vis").eq(2).attr('id','tableMembers');
+$(tribeTable+" tr").eq(rowStart-1).append("<th onclick='sortTableTest("+columnStart+")'>ODA</th><th onclick='sortTableTest("+(columnStart+1)+")'>ODD</th><th onclick='sortTableTest("+(columnStart+2)+")'>ODS</th><th onclick='sortTableTest("+(columnStart+3)+")'>Loot</th><th onclick='sortTableTest("+(columnStart+4)+")'>Gathered</th><th onclick='sortTableTest("+(columnStart+5)+")'>Combined</th>")
+$(tribeTable).eq(rowStart-1).attr('id', 'tableMembers');
 $("#contentContainer").eq(0).prepend(`
                 <div id="progressbar" style="width: 100%;
                 background-color: #36393f;"><div id="progress" style="width: 0%;
@@ -96,12 +133,12 @@ $.getAll(linksODA, (i, data) => {
     else {
         ODAperPlayer.push(["0", "Never"]);
     }
-    
+
 },
     () => {
         $("#progress").css("width", `${(linksODA.length) / linksODA.length * 100}%`);
-        for (var o = 3; o < ODAperPlayer.length + 3; o++) {
-            $("#content_value table.vis tr").eq(o).append("<td title=" + ODAperPlayer[o - 3][1] + ">" + ODAperPlayer[o - 3][0] + "</th>")
+        for (var o = rowStart; o < ODAperPlayer.length + rowStart; o++) {
+            $(tribeTable +" tr").eq(o).append("<td title=" + ODAperPlayer[o - rowStart][1] + ">" + ODAperPlayer[o - rowStart][0] + "</td>")
         }
         //ODD
         $.getAll(linksODD, (i, data) => {
@@ -115,8 +152,8 @@ $.getAll(linksODA, (i, data) => {
 
         },
             () => {
-                for (var o = 3; o < ODDperPlayer.length + 3; o++) {
-                    $("#content_value table.vis tr").eq(o).append("<td title=" + ODDperPlayer[o - 3][1] + ">" + ODDperPlayer[o - 3][0] + "</th>")
+                for (var o = rowStart; o < ODDperPlayer.length + rowStart; o++) {
+                    $(tribeTable +" tr").eq(o).append("<td title=" + ODDperPlayer[o - rowStart][1] + ">" + ODDperPlayer[o - rowStart][0] + "</td>")
                 }
                 //ODS
                 $.getAll(linksODS, (i, data) => {
@@ -130,8 +167,8 @@ $.getAll(linksODA, (i, data) => {
 
                 },
                     () => {
-                        for (var o = 3; o < ODSperPlayer.length + 3; o++) {
-                            $("#content_value table.vis tr").eq(o).append("<td title=" + ODSperPlayer[o - 3][1] + ">" + ODSperPlayer[o - 3][0] + "</th>")
+                        for (var o = rowStart; o < ODSperPlayer.length + rowStart; o++) {
+                            $(tribeTable +" tr").eq(o).append("<td title=" + ODSperPlayer[o - rowStart][1] + ">" + ODSperPlayer[o - rowStart][0] + "</td>")
                         }
 
                         //loot
@@ -146,11 +183,32 @@ $.getAll(linksODA, (i, data) => {
 
                         },
                             () => {
-                                for (var o = 3; o < lootperPlayer.length + 3; o++) {
-                                    $("#content_value table.vis tr").eq(o).append("<td title=" + lootperPlayer[o - 3][1] + ">" + lootperPlayer[o - 3][0] + "</th>")
+                                for (var o = rowStart; o < lootperPlayer.length + rowStart; o++) {
+                                    $(tribeTable +" tr").eq(o).append("<td title=" + lootperPlayer[o - rowStart][1] + ">" + lootperPlayer[o - rowStart][0] + "</td>")
                                 }
+                                //gathering
+                                $.getAll(linksGathering, (i, data) => {
+                                    if ($(data).find(".lit-item")[3] != undefined) {
+                                        temp = $(data).find(".lit-item")
+                                        gatheredperPlayer.push([temp[3].innerText, temp[4].innerText]);
+                                    }
+                                    else {
+                                        gatheredperPlayer.push(["0", "Never"]);
+                                    }
 
-                                $("#progress").remove();
+                                },
+                                    () => {
+                                        for (var o = rowStart; o < gatheredperPlayer.length + rowStart; o++) {
+                                            $(tribeTable +" tr").eq(o).append("<td title=" + gatheredperPlayer[o - rowStart][1] + ">" + gatheredperPlayer[o - rowStart][0] + "</td>")
+                                            $(tribeTable +" tr").eq(o).append("<td>" + (parseInt(gatheredperPlayer[o - rowStart][0].replace(".", ""))+ parseInt(lootperPlayer[o - rowStart][0].replace(".", ""))) + "</td>")
+                                        }
+
+                                        $("#progress").remove();
+
+                                    },
+                                    (error) => {
+                                        console.error(error);
+                                    });
                             },
                             (error) => {
                                 console.error(error);
@@ -175,57 +233,57 @@ $.getAll(linksODA, (i, data) => {
 
 
 
-    function sortTableTest(n) {
-        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-        table = document.getElementById("tableMembers");
-        switching = true;
-        // Set the sorting direction to ascending:
-        dir = "asc";
-        /* Make a loop that will continue until
-        no switching has been done: */
-        while (switching) {
-            // Start by saying: no switching is done:
-            switching = false;
-            rows = table.rows;
-            /* Loop through all table rows (except the
-            first, which contains table headers): */
-            for (i = 1; i < (rows.length - 1); i++) {
-                // Start by saying there should be no switching:
-                shouldSwitch = false;
-                /* Get the two elements you want to compare,
-                one from current row and one from the next: */
-                x = rows[i].getElementsByTagName("td")[n];
-                y = rows[i + 1].getElementsByTagName("td")[n];
-                /* Check if the two rows should switch place,
-                based on the direction, asc or desc: */
-                if (dir == "asc") {
-                    if (Number(x.innerHTML.replace(/\./g,'')) > Number(y.innerHTML.replace(/\./g,''))) {
-                        // If so, mark as a switch and break the loop:
-                        shouldSwitch = true;
-                        break;
-                    }
-                } else if (dir == "desc") {
-                    if (Number(x.innerHTML.replace(/\./g,'')) < Number(y.innerHTML.replace(/\./g,''))) {
-                        // If so, mark as a switch and break the loop:
-                        shouldSwitch = true;
-                        break;
-                    }
+function sortTableTest(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("tableMembers");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir == "asc") {
+                if (Number(x.innerHTML.replace(/\./g, '')) > Number(y.innerHTML.replace(/\./g, ''))) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
                 }
-            }
-            if (shouldSwitch) {
-                /* If a switch has been marked, make the switch
-                and mark that a switch has been done: */
-                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                switching = true;
-                // Each time a switch is done, increase this count by 1:
-                switchcount++;
-            } else {
-                /* If no switching has been done AND the direction is "asc",
-                set the direction to "desc" and run the while loop again. */
-                if (switchcount == 0 && dir == "asc") {
-                    dir = "desc";
-                    switching = true;
+            } else if (dir == "desc") {
+                if (Number(x.innerHTML.replace(/\./g, '')) < Number(y.innerHTML.replace(/\./g, ''))) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
                 }
             }
         }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
     }
+}
