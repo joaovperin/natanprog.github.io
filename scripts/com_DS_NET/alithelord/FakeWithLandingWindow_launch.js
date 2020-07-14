3d0e3e9661d29aa36546276ba2fe83dd392270ef
natanprog <natan10@ymail.com>
2020-07-14 16:00:00 -0300

    Atualizado Fake Script with Landing Window AliTheLord

--- scripts/com_DS_NET/alithelord/FakeWithLandingWindow_launch.js
+++ scripts/com_DS_NET/alithelord/FakeWithLandingWindow_launch.js
@@ -1,6 +1,6 @@
 var land = "July 16, 2020 07:00:00";
 var landWindow = [2, 0, 0];
 
-var coords = '395|352';
+var coords = '300|369';
 
 $.getScript('https://natanprog.github.io/scripts/com_DS_NET/alithelord/FakeWithLandingWindow.js');
