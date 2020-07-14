f1d94fccab07f27510ddd396643f4e72764182d7
natanprog <natan10@ymail.com>
2020-07-14 15:43:12 -0300

    Atualizado Fake Script with Landing Window AliTheLord

---
+++ scripts/com_DS_NET/alithelord/FakeWithLandingWindow_quickbar.js
@@ -0,0 +1,18 @@
+javascript:
+
+var allowed = new Array();
+allowed['spear'] = true;
+allowed['sword'] = true;
+allowed['axe'] = true;
+allowed['archer'] = true;
+allowed['spy'] = true;
+allowed['light'] = true;
+allowed['marcher'] = true;
+allowed['heavy'] = true;
+allowed['ram'] = true;
+allowed['catapult'] = true;
+allowed['snob'] = false;
+
+$.getScript('https://natanprog.github.io/scripts/com_DS_NET/alithelord/FakeWithLandingWindow_launch.js');
+
+void(0);
